63a3f9f42858a667b242e5dba5eaa4a9a8a76a7d
Mary <ngangamary078@gmail.com>
2023-01-17 16:40:51 +0300

    added some insertion

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
 // Import express package
 const express = require('express');
 
+let indexRouter = require('./routes/index');
 
 // Initialize express
 const app = express();
@@ -12,14 +14,13 @@ app.set('view engine', 'ejs');
 // Set a static folder
 app.use(express.static('public'));
 
-app.get('/', (req,res)=>{
-    res.render('index');
- })
+// Define the index router
+app.use('/', indexRouter);
 
 
 // Define the port number
 const PORT = 5000;
 
 app.listen(PORT, ()=>{
-    console.log(`Server is listening on port ${PORT}`)
+   console.log(`Server is listening on port ${PORT}`)
 });
